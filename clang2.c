32ebd63540d58ca7e4e67d07d95fd6a0ac4c5277
V.Namaswi <vajjalanamaswi@gmail.com>
2025-08-19 14:35:39 +0530

    Update clang2.c

--- clang2.c
+++ clang2.c
@@ -1,17 +1,23 @@
-#include <stdio.h>
-  
-   int main() {
-   float circumference=154;
-  
-    float pi=22/7;
-       float radius=circumference/(2*pi);                
-       circumference=2*pi*radius;                       
- 
-     printf("Radius:%lf",radius);
- 
-      float area=pi*radius*radius;
-        printf("Area=%lf",area);
- 
- 
-     return 0;
-  }
+ #include <stdio.h>
+
+
+void calculate(float circumference);
+
+int main() {
+    float circumference = 154.0;
+
+    
+    calculate(circumference);
+
+    return 0;
+}
+
+
+void calculate(float circumference) {
+    float pi = 22.0 / 7;  // Correct float division
+    float radius = circumference / (2 * pi);
+    float area = pi * radius * radius;
+
+    printf("Radius: %.2f\n", radius);
+    printf("Area: %.2f\n", area);
+}
